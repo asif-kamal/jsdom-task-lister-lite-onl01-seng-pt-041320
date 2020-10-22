a719ecd56963e9c1e7a3d8c8a880db9d7b70af2e
asif-kamal <asif.kamal1489@gmail.com>
2020-10-22 19:34:17 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  // const tasks = document.getElementById('tasks');
+  const form = document.getElementById('create-task-form');
+  
+  form.addEventListener('submit', function(event) {
+    const tasks = document.getElementById('tasks');
+    let li = document.createElement('li')
+    let data = document.getElementById('new-task-description').value
+    let node = document.createTextNode(data)
+    li.appendChild(node)
+    tasks.appendChild(li)
+    event.preventDefault();
+  });
 });
