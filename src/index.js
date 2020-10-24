5c91e710405ec6c70b9519644f526de9d5bc2436
asif-kamal <asif.kamal1489@gmail.com>
2020-10-23 20:56:37 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 
-  // const tasks = document.getElementById('tasks');
+  const tasks = document.getElementById('tasks');
   const form = document.getElementById('create-task-form');
   
+  
   form.addEventListener('submit', function(event) {
-    const tasks = document.getElementById('tasks');
-    let li = document.createElement('li')
+    //const tasks = document.getElementById('tasks');
+    let li = document.createElement('li');
     let data = document.getElementById('new-task-description').value
     let node = document.createTextNode(data)
     li.appendChild(node)
     tasks.appendChild(li)
+    let btn = document.createElement("button")
+    btn.innerHTML = "X"
+    li.appendChild(btn)
     event.preventDefault();
+
+    btn.addEventListener('click', function(event) {
+      li.remove()
+    
+    });
   });
+
+
+  // const btn = document.querySelector('button')
+
+  // btn.addEventListener('click', function(event) {
+  //   li.remove()
+  
+  // });
+
 });
